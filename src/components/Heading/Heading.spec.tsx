ffdb88ac50ce9014cc5dcd414fbae70ef2541c3f
Carlos <carlosgamboa@mail.com>
2022-08-22 18:48:46 +0200

    add global styles & reset

--- src/components/Heading/Heading.spec.tsx
+++ src/components/Heading/Heading.spec.tsx
@@ -2,6 +2,7 @@ import { render } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import Heading from './Heading'
 
+// Test ============================================================================
 test('Heading component display the children string properly', () => {
   const testString = 'Testing'
   const { getByRole } = render(<Heading>{testString}</Heading>)
--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -1,11 +1,8 @@
 import { FC } from 'react'
 import styles from './Heading.module.scss'
+
 const Heading: FC<{ children: string }> = ({ children }) => {
-  return (
-    <div>
-      <h1 className={styles.headingStyle}>{children}</h1>
-    </div>
-  )
+  return <h1 className={styles.headingStyle}>{children}</h1>
 }
 
 export default Heading
